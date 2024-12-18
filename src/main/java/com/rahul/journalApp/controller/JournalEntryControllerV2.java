dbbf8a54868ee4d12e43cec76bd288b5f7f198e9
Rahul <86316159+rahul17081998@users.noreply.github.com>
2024-12-18 17:28:27 +0530

    Handling HttpStatus while creating REST API

--- src/main/java/com/rahul/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/rahul/journalApp/controller/JournalEntryControllerV2.java
@@ -6,6 +6,8 @@ import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -20,38 +22,54 @@ public class JournalEntryControllerV2 {
     private JournalEntryService journalEntryService;
 
     @GetMapping
-    public List<JournalEntity> getAllJournals(){
+    public ResponseEntity<List<JournalEntity>> getAllJournals(){
         logger.info("Fetching all journals");
-        return journalEntryService.getAllJournal();
+        List<JournalEntity> allJournal= journalEntryService.getAllJournal();
+        if(allJournal !=null && !allJournal.isEmpty()){
+            logger.info("> No of journals: {}", allJournal.size());
+            return new ResponseEntity<>(allJournal, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public JournalEntity createJournal(@RequestBody JournalEntity myJournal){
-        return journalEntryService.saveJournalEntry(myJournal);
+    public ResponseEntity<JournalEntity> createJournal(@RequestBody JournalEntity myJournal){
+        try{
+            JournalEntity savedJournalEntry=journalEntryService.saveJournalEntry(myJournal);
+            return new ResponseEntity<>(savedJournalEntry, HttpStatus.CREATED);
+        }catch (Exception e){
+            logger.info("Exception occur while creating a journal entry, message: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @GetMapping("/id/{id}")
-    public JournalEntity getJournalById(@PathVariable("id") ObjectId id){
+    public ResponseEntity<JournalEntity> getJournalById(@PathVariable("id") ObjectId id){
         logger.info("Fetching journal with id: {}", id);
         Optional<JournalEntity> optionalJournalEntity = journalEntryService.findJournalById(id);
-        return optionalJournalEntity.orElse(null);
+        if(optionalJournalEntity.isPresent()){
+            return new ResponseEntity<>(optionalJournalEntity.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/id/{id}")
-    public String deleteJournal(@PathVariable("id") ObjectId id){
+    public ResponseEntity<?> deleteJournal(@PathVariable("id") ObjectId id){
         journalEntryService.deleteJournalById(id);
-        return "deleted";
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/id/{id}")
-    public JournalEntity updateJournal(@PathVariable("id") ObjectId id, @RequestBody JournalEntity newJournal){
+    public ResponseEntity<JournalEntity> updateJournal(@PathVariable("id") ObjectId id, @RequestBody JournalEntity newJournal){
         Optional<JournalEntity> optionalJournalEntity = journalEntryService.findJournalById(id);
         JournalEntity oldJournal = optionalJournalEntity.orElse(null);
         if(oldJournal!=null){
             oldJournal.setTitle(newJournal.getTitle()!=null && !newJournal.getTitle().equals("")? newJournal.getTitle() : oldJournal.getTitle());
             oldJournal.setContent(newJournal.getContent()!=null && !newJournal.getContent().equals("")? newJournal.getContent() : oldJournal.getContent());
+            journalEntryService.saveJournalEntry(oldJournal);
+            return new ResponseEntity<>(oldJournal, HttpStatus.OK);
         }
-        journalEntryService.saveJournalEntry(oldJournal);
-        return oldJournal;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
