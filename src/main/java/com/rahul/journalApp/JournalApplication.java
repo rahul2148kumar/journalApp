dee8cd93980ac750a1e8dd79b238ce420f2832be
Rahul <86316159+rahul17081998@users.noreply.github.com>
2024-12-22 22:11:18 +0530

    Added Logger to view the current active running environment

--- src/main/java/com/rahul/journalApp/JournalApplication.java
+++ src/main/java/com/rahul/journalApp/JournalApplication.java
@@ -1,13 +1,20 @@
 package com.rahul.journalApp;
 
+import com.rahul.journalApp.controller.JournalEntryControllerV2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JournalApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(JournalEntryControllerV2.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(JournalApplication.class, args);
+		ConfigurableApplicationContext context =SpringApplication.run(JournalApplication.class, args);
+		logger.info("> Active Environment: {}", context.getEnvironment().getActiveProfiles()[0]);
 	}
 
 }
