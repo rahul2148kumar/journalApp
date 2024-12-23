e7e045efa06b538f7bb9aa672ae19e1073f488eb
Rahul <86316159+rahul17081998@users.noreply.github.com>
2024-12-24 03:15:39 +0530

    Sending email using spring boot

---
+++ src/test/java/com/rahul/journal_app/service/EmailServiceTest.java
@@ -0,0 +1,20 @@
+package com.rahul.journal_app.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+
+    @Test
+    public void testSendEmail(){
+        emailService.sendMail("rahul2140kumar@gmail.com",
+                "Testing java mail sender",
+                "Hi, Kya hal chal");
+    }
+}
