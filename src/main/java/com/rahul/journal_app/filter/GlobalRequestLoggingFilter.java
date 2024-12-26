0143d19b8bd107b656bd0176a65a7128f76c4019
Rahul <86316159+rahul17081998@users.noreply.github.com>
2024-12-26 19:17:03 +0530

    Added Global Request Logging Filter to show  the HttpsRequest in log

---
+++ src/main/java/com/rahul/journal_app/filter/GlobalRequestLoggingFilter.java
@@ -0,0 +1,25 @@
+package com.rahul.journal_app.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Slf4j
+@Component
+@WebFilter("/*") // Applies to all requests
+public class GlobalRequestLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        String httpMethod = httpRequest.getMethod();
+        String requestUri = httpRequest.getRequestURI();
+
+        log.info("HttpMethod: {} :: Path: {}", httpMethod, requestUri);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
