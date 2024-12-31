5886d9b1755b08b74280ff4776bcd85e32ac809f
Rahul <86316159+rahul17081998@users.noreply.github.com>
2025-01-01 01:22:21 +0530

    added remove admin access and grant admin access to the user

--- src/main/java/com/rahul/journal_app/controller/AdminController.java
+++ src/main/java/com/rahul/journal_app/controller/AdminController.java
@@ -1,16 +1,20 @@
 package com.rahul.journal_app.controller;
 
 import com.rahul.journal_app.cache.AppCache;
+import com.rahul.journal_app.constants.Constants;
 import com.rahul.journal_app.entity.User;
 import com.rahul.journal_app.model.UserDto;
+import com.rahul.journal_app.repository.UserRepository;
 import com.rahul.journal_app.service.UserService;
+import com.rahul.journal_app.utils.Util;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -20,6 +24,12 @@ public class AdminController {
 
     @Autowired
     private AppCache appCache;
+
+    @Autowired
+    private Util util;
+
+    @Autowired
+    private UserRepository userRepository;
 
 
 
@@ -39,10 +49,70 @@ public class AdminController {
 
     @PostMapping("/add-admin")
     public ResponseEntity<?> addAdmin(@RequestBody User user){
+        if(!util.isValidEmail(user.getUserName())){
+            return new ResponseEntity<>(Constants.INVALID_EMAIL_FORMAT, HttpStatus.BAD_REQUEST);
+        }
+        User dbuser=userRepository.findByUserName(user.getUserName());
+        if(dbuser!=null){
+            return new ResponseEntity<>(Constants.USER_ALREADY_EXIST, HttpStatus.BAD_REQUEST);
+        }
         Boolean isUserAdded=userService.addAdmin(user);
         if(isUserAdded!=null && isUserAdded){
             return new ResponseEntity<>(user.getUserName()+" added with Admin Access", HttpStatus.CREATED);
         }
         return new ResponseEntity<>("An error occur to add the user", HttpStatus.NOT_ACCEPTABLE);
     }
+
+    @PostMapping("/grant-admin-access")
+    public ResponseEntity<?> grantAdminAccessToUser(@RequestParam("email") String userName){
+        if(!util.isValidEmail(userName)){
+            return new ResponseEntity<>(Constants.INVALID_EMAIL_FORMAT, HttpStatus.BAD_REQUEST);
+        }
+        User user = userRepository.findByUserName(userName);
+        if(user==null){
+            return new ResponseEntity<>(Constants.USER_NOT_FOUND, HttpStatus.BAD_REQUEST);
+        }
+        if(util.isAdmin(userName)){
+            return new ResponseEntity<>(Constants.USER_ALREADY_HAS_ADMIN_ACCESS, HttpStatus.BAD_REQUEST);
+        }
+
+        try{
+            ResponseEntity<?> response = userService.updateRoleOfUser(user, true);
+            if(response.getBody()!=null){
+                return response;
+            }
+        }catch (Exception e){
+            log.error("Exception, while granting admin access to the user {}", e.getMessage() ,e);
+            throw new RuntimeException(e);
+        }
+        return new ResponseEntity<>(Constants.ADMIN_ACCESS_GRANT_EXCEPTION, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @PostMapping("/remove-admin-access")
+    public ResponseEntity<?> removeAdminAccess(@RequestParam("email") String userName){
+        if(!util.isValidEmail(userName)){
+            return new ResponseEntity<>(Constants.INVALID_EMAIL_FORMAT, HttpStatus.BAD_REQUEST);
+        }
+        User user = userRepository.findByUserName(userName);
+        if(user==null){
+            return new ResponseEntity<>(Constants.USER_NOT_FOUND, HttpStatus.BAD_REQUEST);
+        }
+        if(!util.isAdmin(userName)){
+            return new ResponseEntity<>(Constants.USER_DOES_NOT_HAVE_ADMIN_ACCESS, HttpStatus.BAD_REQUEST);
+        }
+
+        try{
+            ResponseEntity<?> response = userService.updateRoleOfUser(user, false);
+            if(response.getBody()!=null){
+                return response;
+            }
+        }catch (Exception e){
+            log.error("Exception, while granting admin access to the user {}", e.getMessage() ,e);
+            throw new RuntimeException(e);
+        }
+        return new ResponseEntity<>(Constants.ADMIN_ACCESS_REMOVE_EXCEPTION, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
